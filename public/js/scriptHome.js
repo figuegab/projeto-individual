3cbbfdaff4b6daf5bc3db8978ae18d7d041be267
figuegab <gabriel.figueiredo@sptech.school>
2025-06-02 14:15:36 -0300

    fix: :bug: Correção de eixbição do usuario logado

--- public/js/scriptHome.js
+++ public/js/scriptHome.js
@@ -1,6 +1,6 @@
 let span_usuario_logado = document.getElementById("span_usuario_logado");
 
-if (!sessionStorage.NOME_USER) {
+if (sessionStorage.NOME_USER) {
   console.log(`Nome do usuário: ${sessionStorage.NOME_USER}`);
   span_usuario_logado.style.display = "none";
 } else {
